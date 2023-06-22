6011d1f0e13db2b1021acf74fc675075e2ce52b0
sarahpuspdew <sarahpuspadewi.spd@gmail.com>
2023-06-22 16:15:45 +0700

    Add variables and accessors

--- Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages a collection of flower plants and attached to flowers
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // The diameter of the area where the agent and flowers can be
+    // used for observing distance from agent to flower
+    public const float AreaDiameter = 20f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // The list of all the flower plants in this flower area (flower area have multiple flowers)
+    private List<GameObject> flowerPlants;
+
+    // A Lookup dictionary for looking up a flower from a nectar collider
+    private Dictionary<Collider, Flower> nectarFlowerDictionary;
+
+    // The list of all flowers in the flower area
+    public List<Flower> Flowers { get; private set; }
 }
